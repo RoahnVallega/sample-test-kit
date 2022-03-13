bccfc211c7a8852ba346595d94a0f4eaf4b37428
Roahn Vinsaint Vallega <99853540+RoahnVallega@users.noreply.github.com>
2022-03-13 16:19:14 +0800

    done activity

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,82 +1,76 @@
 import React from "react";
-import { View, Image, Text, Dimensions } from "react-native";
+import {StyleSheet,View,Text, TextInput,TouchableOpacity, Image} from  "react-native";
 
 export default function TabOneScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#273746',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <View style={{
-        height: Dimensions.get('screen').height * 0.5,
-        width: '80%',
-        backgroundColor: 'white',
-        padding: 10,
-        borderRadius: 10
-      }}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'red',
-            marginBottom: 10,
-            borderRadius: 10,
-            overflow: 'hidden'
-          }}
-        >
-          <Image
-            style={{
-              height: '100%',
-              width: '100%',
-              resizeMode: 'cover'
-            }}
-            source={require('../assets/images/goldenwind.jpg')}
-          />
-        </View>
-        <View
-          style={{
-            flex: 0,
-            backgroundColor: '#E9DBD8',
-            paddingVertical: 10,
-            paddingHorizontal: 10,
-            marginBottom: 10,
-            borderRadius: 10,
-          }}
-        >
-          <Text
-            style={{
-              color: '#877D7B',
-              fontFamily: 'poppins-bold',
-              fontSize: 18,
-            }}
-            numberOfLines={1}
-          >
-            My NFT Card
-          </Text>
-        </View>
-        <View
-          style={{
-            flex: 0,
-            minHeight: 100,
-            maxHeight: 200,
-            backgroundColor: 'white',
-            padding: 5,
-          }}
-        >
-          <Text
-            style={{
-              fontFamily: 'poppins-regular',
-              color: '#877D7B',
-              fontSize: 14
-            }}
-          >
-            Golden Wind, also known as Vento Aureo, is the fifth story arc of the Japanese manga series JoJo's Bizarre Adventure, written and illustrated by Hirohiko Araki. It was serialized in Weekly Shōnen Jump for a little under 4 years, from November 20, 1995, to April 5, 1999.
-          </Text>
-        </View>
-      </View>
+    <View style={styles.container}>
+      <Image source = {{uri:'https://i1.sndcdn.com/artworks-000505868481-hdd64e-t500x500.jpg'}}
+      style={styles.imageIcon}/>
+      <Text style={styles.Login}>Welcome! Stand-User</Text>
+      <TextInput style={styles.inputText}
+        placeholder= "Enter Username"
+      />
+      <TextInput style={styles.inputText}
+        placeholder= "Enter Password"
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.btnLogin}>
+        <Text style={styles.btnText}>Login</Text>
+      </TouchableOpacity>
+      <Text style={styles.basictext}>No Account yet?</Text>
+      <TouchableOpacity style={styles.btnSignUp}>
+        <Text style={styles.btnText}>Sign-up</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  backgroundColor: '#000000',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontFamily: 'poppins-regular'
+},
+Login: {
+ fontSize: 30,
+  textAlign: 'center',
+  margin: 18,
+  fontFamily: 'poppins-bold',
+  color: 'white'
+},
+inputText:{
+  width: "90%",
+  backgroundColor:'#FFFFFF',
+  padding: 15,
+  marginBottom: 10,
+  color: '#333D29'
+},
+btnLogin: {
+  backgroundColor: '#FFFF00',
+  padding: 15,
+  width: '45%',
+  justifyContent: 'center',
+  marginBottom: 30
+},
+btnSignUp: {
+  backgroundColor: '#808000',
+  padding: 15,
+  width: '30%',
+  justifyContent: 'center'
+},
+btnText: {
+  fontSize: 16,
+  textAlign: 'center',
+  color: '#0f0f0f',
+  fontFamily: 'poppins-bold'
+},
+imageIcon: {     
+  width: 100, 
+  height: 100 
+},
+basictext: {
+  fontFamily: 'poppins-semi-bold'
+}
+});
